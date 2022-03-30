8384bd5654cfb82001f900a50049fc661ec2230d
felix medina lopez <felix.medina.lopez1@gmail.com>
2022-03-30 00:40:03 -0300

    bootstrap & component1

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { BsFillCartFill } from "react-icons/bs";
+const NavBar = () => {
+return <>
+  <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      < BsFillCartFill color='white' />
+    </Nav>
+    </Container>
+  </Navbar>
+  
+</>
+}
+
+export default NavBar;
